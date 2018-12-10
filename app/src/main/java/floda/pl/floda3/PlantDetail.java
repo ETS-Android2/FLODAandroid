c8686f21a664f996a688dbfeb7b090be62abf6c8
delowe <bartoszadamczyk3@gmail.com>
2018-12-10 23:03:52 +0100

    1 part of detail

--- app/src/main/java/floda/pl/floda3/PlantDetail.java
+++ app/src/main/java/floda/pl/floda3/PlantDetail.java
@@ -3,16 +3,22 @@ package floda.pl.floda3;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import com.github.mikephil.charting.charts.LineChart;
 
 public class PlantDetail extends AppCompatActivity {
-
+    String id;
+    LineChart nawodnienie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant_detail);
         Intent i = getIntent();
-        String id= i.getStringExtra("ID");
-        Log.e("e",id);
+        id= i.getStringExtra("ID");
+        Toolbar t = findViewById(R.id.plantdettool);
+        setSupportActionBar(t);
+
+        nawodnienie= (LineChart) findViewById(R.id.nawodnienie);
     }
 }
